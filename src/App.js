7ca487b21a6afc41d0d64614613d1bbfd3e8568c
OmarElaraby26 <omarelaraby26@gmail.com>
2022-09-01 16:56:49 +0200

    add App.js & index.js files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Nav />
+
+      <Header />
+
+      <Main />
+    </React.Fragment>
+  );
+}
+
+export default App;
