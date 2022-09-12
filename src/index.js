376795ef4e380b5d22eb312c7b736dab2268b126
OmarElaraby26 <omarelaraby26@gmail.com>
2022-09-12 18:18:14 +0200

    create routing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 import './css/index.css';
 
-import App from './App';
+import Home from './pages/Home';
+import Course from './pages/Course';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/course/:id" element={<Course />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
